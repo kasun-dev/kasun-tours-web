a21cfb4db5be821b8cf3c4bf8a5d32e455898473
Kasun Sampath <ks2005755@gmail.com>
2025-07-07 13:12:38 +0530

    load screen commit

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,38 +1,17 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import NavBar from "@/components/NavBar"; // Import NavBar component
-import Footer from "@/components/Footer"; // Import Footer component
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// app/layout.tsx
+import type { Metadata } from 'next'
+import './globals.css'
+import ClientLayout from './clientLayout'
 
 export const metadata: Metadata = {
-  title: "Kasun Tours",
-  description: "Travel with Kasun Tours-web-app",
-};
+  title: 'Kasun Tours',
+  description: 'Travel with Kasun Tours-web-app',
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <NavBar/> 
-        {children}
-        <Footer/>
-      </body>
-        
-      
+      <ClientLayout>{children}</ClientLayout>
     </html>
-  );
+  )
 }
