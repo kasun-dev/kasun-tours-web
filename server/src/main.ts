c8735c64fccae64f52169780c2eb97fd12d39805
Kasun Sampath <ks2005755@gmail.com>
2025-07-15 11:19:15 +0530

    config env commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5002);
+  console.log('Backend URL-PORT:', process.env.PORT);
 }
 bootstrap();
